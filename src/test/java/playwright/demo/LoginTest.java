0336a3db291fefddab13bf287e00c05632711d15
Shwetha Acharya <DK2530@desicrew.in>
2025-08-05 20:34:31 +0530

    Day4 - waits

---
+++ src/test/java/playwright/demo/LoginTest.java
@@ -0,0 +1,30 @@
+package playwright.demo;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+
+public class LoginTest {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+
+            page.navigate("https://practicetestautomation.com/practice-test-login/");
+            // page.waitForLoadState(LoadState.LOAD); // Wait for full page load
+
+            page.waitForSelector("#username").fill("student");
+            page.fill("#password", "Password123");
+
+            page.click("#submit");
+
+            // Wait for URL to confirm login success
+            page.waitForURL("**/logged-in-successfully/");
+
+            // Assert login was successful
+            String successMsg = page.textContent("h1");
+            System.out.println("Login Message: " + successMsg);
+
+            browser.close();
+        }
+    }
+}
