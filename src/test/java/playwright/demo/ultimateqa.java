8ef98f652e895e1c4e45afe66033e2807c5e94ac
Shwetha Acharya <DK2530@desicrew.in>
2025-08-30 11:51:44 +0530

    Final

--- src/test/java/playwright/demo/ultimateqa.java
+++ src/test/java/playwright/demo/ultimateqa.java
@@ -2,7 +2,9 @@ package playwright.demo;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Page.LocatorOptions;
 import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.LoadState;
 import com.microsoft.playwright.options.WaitUntilState;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,11 +24,6 @@ public class ultimateqa {
     static void setup(){
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-
-    }
-
-       @BeforeEach
-    void createContextAndPage() {
         BrowserContext context = browser.newContext();
         context = browser.newContext();
         page = context.newPage();
@@ -37,16 +34,23 @@ public class ultimateqa {
         options.waitUntil = WaitUntilState.LOAD;
 
         page.navigate("https://courses.ultimateqa.com/collections", options);
+
     }
 
-      @AfterEach
-    void cleanup() {
-        BrowserContext context = browser.newContext();
-        context.close();
-    }
+    //    @BeforeEach
+    // void createContextAndPage() {
+        
+    // }
+
+    //   @AfterEach
+    // void cleanup() {
+       
+    // }
 
     @AfterAll
     static void teardown() {
+        BrowserContext context = browser.newContext();
+        context.close();
         browser.close();
         playwright.close();
     }
@@ -83,4 +87,78 @@ public class ultimateqa {
         Emailerror.waitFor();
         assertEquals("Please enter a valid email address", Emailerror.innerText());
     }
+
+
+      @Test
+    void TestCase3() {
+        Locator signInLink = page.locator("a[href='/users/sign_in']");
+        signInLink.click(new Locator.ClickOptions().setForce(true));
+
+        Locator Emailfield = page.locator("input[id='user[email]']");
+        Emailfield.click();
+        Emailfield.fill("Shwetha@gmail.com");
+
+        Locator Passfield = page.locator("input[id='user[password]']");
+        Passfield.click();
+        Passfield.fill("Shwetha@28");
+
+        Locator Rememberme = page.locator("input[id='user[remember_me]']");
+        Rememberme.check();
+
+        Locator SignInButton = page.locator("//button[@type='submit']");
+        SignInButton.click();
+
+    }
+
+    @Test
+    void createAccount() {
+
+        Locator signInLink = page.locator("a[href='/users/sign_in']");
+        signInLink.click(new Locator.ClickOptions().setForce(true));
+        
+       page.locator("a[href='/users/sign_up']").click();
+
+// Wait for navigation
+page.waitForURL("**/users/sign_up", new Page.WaitForURLOptions().setTimeout(60000));
+page.locator("#user_accepts_terms").check();
+
+
+// Assert URL
+assertTrue(page.url().startsWith("https://courses.ultimateqa.com/users/sign_up"));
+
+// Assert heading text
+String headingText = page.locator("h1.page__heading").innerText();
+System.out.println("Heading text: " + headingText);
+assertTrue(headingText.contains("Create a new account"));
+
+
+
+        Locator firstNameLabel = page.locator("label[for='user[first_name]']");
+        firstNameLabel.waitFor();
+        assertTrue(firstNameLabel.isVisible());
+        page.locator("input[id='user[first_name]']").fill("Shwetha");
+
+        Locator lastNameLabel = page.locator("label[for='user[last_name]']");
+        lastNameLabel.waitFor();
+        assertTrue(lastNameLabel.isVisible());
+        page.locator("input[id='user[last_name]']").fill("Acharya");
+
+        Locator EmailLabel = page.locator("label[for='user[email]']");
+        EmailLabel.waitFor();
+        assertTrue(EmailLabel.isVisible());
+        page.locator("input[id='user[email]']").fill("shwetha@gmail.com");
+
+        Locator PasswordLabel = page.locator("label[for='user[password]']");
+        PasswordLabel.waitFor();
+        assertTrue(PasswordLabel.isVisible());
+        page.locator("input[id='user[password]']").fill("Shwetha@28");
+
+        page.locator("input[id='user[terms]']").uncheck();
+
+        page.locator("input[id='user[terms]']").check();
+
+        page.locator("button[type='submit']").click();
+
+    }
 }
+ 
